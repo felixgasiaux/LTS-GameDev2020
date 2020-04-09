b073cf18e209df9a5272d1436c3e350fbc040bef
Felix Gasiaux <felixgasiaux@iclod.com>
2020-04-09 15:48:05 +0200

    Addition of Gun

--- Unity LTS GameDev/Assets/Scripts/PlayerMovement.cs
+++ Unity LTS GameDev/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,17 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
+    public Animator animator; 
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
     private void FixedUpdate()
     {
