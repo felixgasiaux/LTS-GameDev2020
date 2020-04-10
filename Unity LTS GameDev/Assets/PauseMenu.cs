9aeaa5db7e86b1680fb1f7d8f3a4f8aaef13e9ca
Felix Gasiaux <felixgasiaux@iclod.com>
2020-04-10 19:18:02 +0200

    Changes

--- Unity LTS GameDev/Assets/PauseMenu.cs
+++ Unity LTS GameDev/Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -22,21 +23,33 @@ public class PauseMenu : MonoBehaviour
                 Pause();
             }
         }
+    }
 
 
-        void Resume()
-        {
-            PauseMenuUI.SetActive(false);
-            Time.timeScale = 1f;
-            GameIsPaused = false;
-        }
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
 
-        void Pause()
-        {
-            PauseMenuUI.SetActive(true);
-            Time.timeScale = 0f;
-            GameIsPaused = true;
-        }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu ...");
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
 
     }
 }
