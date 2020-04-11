bd8a93cc861d4e246712042335d13b8170229377
Felix Gasiaux <felixgasiaux@iclod.com>
2020-04-11 22:15:33 +0200

    Addition of Door

---
+++ Unity LTS GameDev/Assets/Scripts/Door_Down_Script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door_Down_Script : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Outside");
+        Debug.Log("Player went to the outside");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
