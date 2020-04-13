e8ca165f6599e3c7a07afc4947937b2644a343c3
Felix Gasiaux <felixgasiaux@iclod.com>
2020-04-13 13:34:34 +0200

    Changes

--- Unity LTS GameDev/Assets/Scripts/Vending_Machine_openStore.cs
+++ Unity LTS GameDev/Assets/Scripts/Vending_Machine_openStore.cs
@@ -2,22 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Vending_Machine_openStore : MonoBehaviour
 {
-    public GameObject ShopUI;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Player entered shop area");
+        Shopcanbeopened = true;
     }
+
+    public static bool Shopcanbeopened = false;
+    public GameObject ShopMenuUI;
+    public bool Shopisopen;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Shopcanbeopened == true)
         {
-            Debug.Log("Player opened the shop");
-            ShopUI.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (Shopisopen)
+                {
+                    Resume();
+                    Debug.Log("Shop is closed");
+                }
+                else
+                {
+                    Pause();
+                    Debug.Log("Shop is opened");
+                }
+            }
         }
+    }
+
+    public void Resume()
+    {
+        ShopMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Shopisopen = false;
+    }
+
+    void Pause()
+    {
+        ShopMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Shopisopen = true;
     }
 }
