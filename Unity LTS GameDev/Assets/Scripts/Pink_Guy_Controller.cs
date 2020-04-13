a4ba97cd990380969685daabc3a903b3b06c2106
Felix Gasiaux <felixgasiaux@iclod.com>
2020-04-13 17:33:43 +0200

    Changes

---
+++ Unity LTS GameDev/Assets/Scripts/Pink_Guy_Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pink_Guy_Controller : MonoBehaviour
+{
+    public bool talktopink;
+    public static bool inboxcollider = false;
+    public GameObject PinkGuy_Talk;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player entered Talking area");
+        inboxcollider = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inboxcollider == true)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                Debug.Log("Player is talking to Pink Guy");
+                PinkGuy_Talk.SetActive(true);
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
