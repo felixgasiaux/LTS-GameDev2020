a852cbbd1eab86988627d62344b1cec9418ada86
quentcha <quentcha05@gmail.com>
2020-04-18 16:23:57 +0200

    big changes

---
+++ Unity LTS GameDev/Assets/Scripts/portal_down.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class portal_down : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Outside");
+        Debug.Log("Player went outside");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
