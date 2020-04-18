2d202eb771adfbac90264aea3abc23c45bc1ab94
quentcha <quentcha05@gmail.com>
2020-04-18 13:57:32 +0200

    changes

--- Unity LTS GameDev/Assets/Scripts/MainMenu.cs
+++ Unity LTS GameDev/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Base");
     }
 
     public void QuitGame()
