bb0989b1c7ab0714ef3d681d90e1ed8a61cf430e
quentcha <quentcha05@gmail.com>
2020-04-19 00:39:17 +0200

    easter egg !!!!!!!!!!!! ( just need to add a cancel button )

---
+++ Unity LTS GameDev/Assets/Scripts/back_button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class back_button : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene("Base");
+    }
+}
