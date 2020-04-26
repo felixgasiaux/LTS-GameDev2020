36fde522cdf550a356a0c2b5ba2dc2cd94c29fbe
quentcha <quentcha05@gmail.com>
2020-04-26 11:50:33 +0200

    changes

---
+++ Unity LTS GameDev/Assets/Scripts/LastCinematic.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LastCinematic : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 5f;
+    float distanceTravelled;
+    public GameObject head1;
+    public GameObject head2;
+    public GameObject head3;
+    public GameObject head4;
+    public GameObject head5;
+    public GameObject head6;
+    public Text conversation;
+    public EndOfPathInstruction endOfPathInstruction;
+    public float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        head1.SetActive(false);
+        head2.SetActive(false);
+        head3.SetActive(false);
+        head4.SetActive(false);
+        head5.SetActive(false);
+        head6.SetActive(false);
+        conversation.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+                if (time < 5f)
+                {
+                    conversation.text = "The crew is composed of 6 astronauts female and male";
+                }
+                else if (time < 10f)
+                {
+                    head1.SetActive(true);
+                    conversation.text = "Pilote Lucie";
+                }
+                else if (time < 15f)
+                {
+                    head2.SetActive(true);
+                    conversation.text = "Command pilote Emma";
+                }
+                else if (time < 20f)
+                {
+                    head3.SetActive(true);
+                    conversation.text = "Commander Luc";
+                }
+                else if (time < 25f)
+                {
+                    head4.SetActive(true);
+                    conversation.text = "Command module pilot Martin";
+                }
+                else if (time < 30f)
+                {
+                    head5.SetActive(true);
+                    conversation.text = "Docking module pilot Quentin";
+                }
+                else if (time < 35f)
+                {
+                    head6.SetActive(true);
+                    conversation.text = "Titan module pilot Felix";
+                }
+                else if (time > 504f)
+                {
+                    SceneManager.LoadScene("Menu");
+                }
+
+            distanceTravelled += speed * Time.deltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);// Stop or Loop
+        }
+        }
+
