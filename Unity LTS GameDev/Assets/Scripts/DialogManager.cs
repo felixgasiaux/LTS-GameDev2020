a9f082594999e36297dad2c012c88a19fd2de897
Felix Gasiaux <felixgasiaux@iclod.com>
2020-04-27 02:32:48 +0200

    Dialog

---
+++ Unity LTS GameDev/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogManager : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+    private AudioSource clicksound;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+        clicksound = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+    public void NextSentence()
+    {
+        clicksound.Play();
+        continueButton.SetActive(false);
+        if (index < sentences.Length -1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+
+}
