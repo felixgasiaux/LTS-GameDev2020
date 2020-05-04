304935da7f3d45d1ebe6ada77ca711814a2a1d1e
Felix Gasiaux <felixgasiaux@iclod.com>
2020-05-04 19:03:09 +0200

    Some bs not working

---
+++ Unity LTS GameDev/Assets/Scripts/Blue_Guy_Controller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using PathCreation;
+using TMPro;
+
+public class Blue_Guy_Controller : MonoBehaviour
+{
+    public bool talktopink;
+    public static bool inboxcollider = false;
+    public GameObject PinkGuy_Talk;
+    public GameObject PressEtointeract;
+    public PathCreator pathCreator;
+    public float speed = 0.5f;
+    float distanceTravelled;
+    public TextMeshProUGUI text;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player entered Talking area");
+        inboxcollider = true;
+        PressEtointeract.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Player left Talking area");
+        inboxcollider = false;
+        PressEtointeract.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inboxcollider == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Player is talking to Pink Guy");
+                PinkGuy_Talk.SetActive(true);
+                Time.timeScale = 0f;
+                PressEtointeract.SetActive(false);
+
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+
+        }
+     //movement
+     distanceTravelled += speed * Time.deltaTime;
+     transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+    }
+}
