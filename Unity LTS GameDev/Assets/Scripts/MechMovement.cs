e09a8234382fb17d4d1081eade320dec67a9872d
quentcha <quentcha05@gmail.com>
2020-05-09 13:22:19 +0200

    addition of kamikaze ennemies

--- Unity LTS GameDev/Assets/Scripts/MechMovement.cs
+++ Unity LTS GameDev/Assets/Scripts/MechMovement.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace Pathfinding
 {
     public class MechMovement : MonoBehaviour
     {
+        public int life = 100;
+        public Slider mSlider;
         public float moveSpeed = 3f;
         public string collision;
         public Rigidbody2D rb;
@@ -21,6 +25,12 @@ namespace Pathfinding
         // Update is called once per frame
         void Update()
         {
+            mSlider.value = life;
+            if (life == 0)
+            {
+                SceneManager.LoadScene("Menu");
+                Debug.Log("Player died");
+            }
             // input because reapeated less times
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical");
@@ -44,7 +54,11 @@ namespace Pathfinding
         void OnCollisionEnter2D(Collision2D col)
         {
 
-            collision = col.gameObject.name;
+            Debug.Log(col.gameObject.name);
+            if (col.gameObject.name == "Sprite" || col.gameObject.name == "Sprite (1)" || col.gameObject.name == "Sprite (2)" || col.gameObject.name == "Sprite (3)" || col.gameObject.name == "Sprite (4)" || col.gameObject.name == "Sprite (5)" || col.gameObject.name == "Sprite (6)"  || col.gameObject.name == "Sprite (7)" || col.gameObject.name == "Sprite (8)" || col.gameObject.name == "Sprite (9)" || col.gameObject.name == "Sprite (10)" || col.gameObject.name == "Sprite (11)" || col.gameObject.name == "Sprite (12)" || col.gameObject.name == "Sprite (13)" || col.gameObject.name == "Sprite (14)" || col.gameObject.name == "Sprite (15)")
+            {
+                life -= 10;
+            }
         }
     }
 }
